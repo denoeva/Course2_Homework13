a944a6c05e1b12a3143a29fe015ee5da778cf41f
elena.denoeva <denoeva.elena@skyeng.ru>
2023-06-21 19:54:28 +0300

    Initial commit

---
+++ src/main/java/pro/sky/homework13tests/Homework13testsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homework13tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework13testsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework13testsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/homework13tests/Homework13testsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework13tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework13testsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
